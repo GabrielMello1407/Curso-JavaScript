5ddfafd6c8b0acdb8746d9a138428c72884efb82
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-01-09 13:14:00 -0300

    exercicios condicionais

---
+++ Aula13/ambiente.js
@@ -0,0 +1,16 @@
+console.log('Vai começar...')
+for(c = 1; c<=4 ; c++){
+    console.log(c)
+}
+console.log('Fim!')
+//for(inicialização (var c = 1); teste lógico (c<=10) ; incremento(c++) ){}
+
+
+
+/*
+var c = 1
+while(c <=5){
+    console.log(c)
+    c++
+}*/
+//--------------------------------
